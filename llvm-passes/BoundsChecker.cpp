69ead011fcfd7212228b4738287cb0afffd1b664
Sergei <mailbox.sa02@gmail.com>
2022-01-27 17:46:58 +0100

    Bounds check first 2 tasks done

--- llvm-passes/BoundsChecker.cpp
+++ llvm-passes/BoundsChecker.cpp
@@ -17,20 +17,42 @@ namespace {
         virtual bool runOnModule(Module &M) override;
 
     private:
-        Value* getPointerOffset(GetElementP &F);
+        DataLayout *dl;
 
+        Value *getPointerOffset(GetElementPtrInst *GEP, IRBuilder<> *B, SmallDenseMap<GetElementPtrInst*, Value*, 32> *computedOffsets);
+        Value *getPtrOrigin(GetElementPtrInst *GEP);
+        Value *getPtrSize(Value *origin);
         bool instrumentGEPs(Function &F);
     };
+
+    ConstantInt *sumConstantInts(ConstantInt *a, ConstantInt *b) {
+        APInt cVal = a->getValue() + b->getValue();
+        Constant *c = ConstantInt::get(a->getType(), cVal);
+        return cast<ConstantInt>(c);
+    }
 }
 
 bool BoundsChecker::instrumentGEPs(Function &F) {
+    // Construct an IRBuilder (at a random insertion point) so we can reuse it
+    // every time we need it.
+    IRBuilder<> B(&F.getEntryBlock());
+    SmallDenseMap<GetElementPtrInst*, Value*, 32> computedOffsets;
+
+    bool modified = false;
+
     for (Instruction &II : instructions(F)) {
         if(GetElementPtrInst* GEP = dyn_cast<GetElementPtrInst>(&II)) {
-            LOG_LINE("Got a GEP:  " << GEP);
-            getPointerOffset(*GEP);
+            LOG_LINE("Got a GEP:  " << *GEP);
+            Value* offset = getPointerOffset(GEP, &B, &computedOffsets);
+            LOG_LINE("Got offset: " << *offset);
+            Value* origin = getPtrOrigin(GEP);
+            LOG_LINE("Got origin: " << *origin);
+            Value* originSize = getPtrSize(origin);
+            LOG_LINE("Origin size: " << *originSize);
+            modified = true;
         }
     }
-    return false;
+    return modified;
 }
 
 bool BoundsChecker::runOnModule(Module &M) {
@@ -38,6 +60,8 @@ bool BoundsChecker::runOnModule(Module &M) {
     // function will insert calls to this function for every allocation. This
     // function is written in our runtime (runtime/dummy.c). To see its (LLVM)
     // type, you can check runtime/obj/dummy.ll)
+
+    dl = new DataLayout(&M);
 
     // LLVM wants to know whether we made any modifications to the IR, so we
     // keep track of this.
@@ -53,16 +77,73 @@ bool BoundsChecker::runOnModule(Module &M) {
         instrumentGEPs(F);
     }
 
+    delete dl;
     return Changed;
 }
 
-Value* getPointerOffset(GetElementPtrInst &GEP) {
-    unsigned width = llvm::DataLayout::getIndexTypeSizeInBits(GEP.getType());
-    if (GEP.accumulateConstantOffset()) {
-
-    } else {
-        
+Value* BoundsChecker::getPointerOffset(GetElementPtrInst *GEP, IRBuilder<> *B, SmallDenseMap<GetElementPtrInst*, Value*, 32> *computedOffsets) {
+    if(computedOffsets->count(GEP) == 1) {
+        return computedOffsets->lookup(GEP);
     }
+    
+    Value* ptr = GEP->getOperand(0);
+    Value* off = GEP->getOperand(1);
+
+    if (GetElementPtrInst *GEP2 = dyn_cast<GetElementPtrInst>(ptr)) {
+        Value* off2 = getPointerOffset(GEP2, B, computedOffsets);
+        ConstantInt *c_off = dyn_cast<ConstantInt>(off);
+        ConstantInt *c_off2 = dyn_cast<ConstantInt>(off2);
+
+        if(c_off && c_off2) {
+            ConstantInt *calculated = sumConstantInts(c_off, c_off2);
+            computedOffsets->insert({GEP, calculated});
+            return calculated;
+        } else {
+            LOG_LINE("Creating ADD instruction");
+            B->SetInsertPoint(GEP);
+            Value *inserted = B->CreateAdd(off, off2);
+            computedOffsets->insert({GEP, inserted});
+            return inserted;
+        }
+    } else {
+        computedOffsets->insert({GEP, off});
+        return off;
+    }
+}
+
+Value* BoundsChecker::getPtrOrigin(GetElementPtrInst *GEP) {
+    Value* ptr = GEP->getOperand(0);
+    if(GetElementPtrInst *GEP2 = dyn_cast<GetElementPtrInst>(ptr)) {
+        return getPtrOrigin(GEP2);
+    } else {
+        return ptr;
+    }
+}
+
+Value* BoundsChecker::getPtrSize(Value* origin) {
+    if(AllocaInst *a_inst = dyn_cast<AllocaInst>(origin)) {
+        return a_inst->getArraySize();
+    } else if(Constant *c_origin = dyn_cast<Constant>(origin)) {
+        if(PointerType *pt = dyn_cast<PointerType>(c_origin->getType())) {
+            Type* ptrElemTy = pt->getPointerElementType();
+            if(SequentialType* seq = dyn_cast<SequentialType>(ptrElemTy)) {
+                Value* ret = ConstantInt::get(IntegerType::getInt32Ty(origin->getContext()), seq->getNumElements());
+                return ret;
+            } else {
+                ERROR("Something has PointerType but it does not refer to a SequentialType");
+            }
+        } else {
+            ERROR("Something is a Constant but it has not a PointerType");
+        }
+    } else if(Argument *arg_origin = dyn_cast<Argument>(origin)) {
+        Function *f = arg_origin->getParent();
+        LOG_LINE("Name: " << f->getName());
+        if (f->getName().equals("main")) {
+            LOG_LINE("This function is called main");
+            return f->getArg(0);
+        }
+    }
+    ERROR("Unknown origin type");
 }
 
 // Register the pass with LLVM so we can invoke it with opt. The first argument
